ce93e929f8fb26023a3b7428e44f10956aab15df
Turið Højgaard <tuho@itu.dk>
2022-10-17 07:50:33 +0200

    sett inn tað sum skal connect við DB (er ikki testa)

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Npgsql;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+var cs = "Host=localhost;Username=postgres;Password=s$cret;Database=Assignment4-BDSA";
+
+using var con = new NpgsqlConnection(cs);
+con.Open();
+
+var sql = "SELECT version()";
+
+using var cmd = new NpgsqlCommand(sql, con);
+
+var version = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"PostgreSQL version: {version}");
